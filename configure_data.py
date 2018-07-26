zero mean training stable in GPR, fix variance now

configure_data.py
@@ -2,6 +2,8 @@ import pickle
 import numpy as np 
 import ipdb
 from utils import vec_to_one_hot_matrix
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def get_features_data(contents, features):
@@ -52,7 +54,18 @@ def save_dataset(filename, target_feature):
     with open(save_file, 'wb') as fn:
         pickle.dump(data_dict, fn)
 
-
+def plot_variety_distribution(fl):
+    with open(fl, 'rb') as f:
+        contents = pickle.load(f)
+    varieties = get_features_data(contents, 'variety').squeeze().reshape(15,37)
+    
+    fig, ax = plt.subplots()
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    im = ax.imshow(varieties, cmap=plt.get_cmap('RdBu', 4))
+    fig.colorbar(im, cax=cax, orientation='vertical', ticks=np.arange(4))
+    plt.show()
+    
 if __name__ == '__main__':
     fl = 'data/data_2018.pkl'
     # available features
@@ -68,4 +81,5 @@ if __name__ == '__main__':
     # save_dataset(fl, 'plant_width_mean')
     # save_dataset(fl, 'plant_height_mean(cm)')
     # save_dataset(fl, 'height_aerial(cm)')
-    save_dataset(fl, 'plant_count_mean')
+    # save_dataset(fl, 'plant_count_mean')
+    plot_variety_distribution(fl)

exps/pw.py
@@ -1,9 +1,45 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import ipdb
+
 from env import FieldEnv
 from models import GpytorchGPR, SklearnGPR
-import numpy as np 
-import ipdb
 from utils import draw_plots, zero_mean_unit_variance
-import matplotlib.pyplot as plt
+
+def draw_3_plots(num_rows, num_cols, true_y, y1, y2,
+               title=None, fig=None, ax=None):
+    if fig is None or ax is None:
+        fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+        axt, axp, axv = ax
+
+    axt.set_title('Ground truth')
+    imt = axt.imshow(true_y.reshape(num_rows, num_cols),
+                     cmap='ocean', vmin=true_y.min(), vmax=true_y.max())
+    div = make_axes_locatable(axt)
+    caxt = div.new_horizontal(size='5%', pad=.05)
+    fig.add_axes(caxt)
+    fig.colorbar(imt, caxt, orientation='vertical')
+
+    axp.set_title('Predicted values from simple GP')
+    imp = axp.imshow(y1.reshape(num_rows, num_cols),
+                     cmap='ocean', vmin=true_y.min(), vmax=true_y.max())
+    divm = make_axes_locatable(axp)
+    caxp = divm.new_horizontal(size='5%', pad=.05)
+    fig.add_axes(caxp)
+    fig.colorbar(imp, caxp, orientation='vertical')
+
+    axv.set_title('Predicted values from GP with gene embedding')
+    imv = axv.imshow(y2.reshape(num_rows, num_cols),
+    				 cmap='ocean', vmin=true_y.min(), vmax=true_y.max())
+    divv = make_axes_locatable(axv)
+    caxv = divv.new_horizontal(size='5%', pad=.05)
+    fig.add_axes(caxv)
+    fig.colorbar(imv, caxv, orientation='vertical')
+
+    if title is not None:
+        fig.suptitle(title)
+    return fig
 
 
 def transform_y(y, inverse=False):
@@ -14,7 +50,9 @@ def transform_y(y, inverse=False):
 
 
 def fit_and_eval(gp, x, y, x_train, y_train, **kwargs):
-    gp.fit(x_train, y_train, **kwargs)
+    var = np.full(len(y_train), .05)
+    # var = None
+    gp.fit(x_train, y_train, var, **kwargs)
     mu, std = gp.predict(x, return_std=True)
     rmse = compute_rmse(y, mu)
     return mu, std, rmse
@@ -28,15 +66,16 @@ def compute_rmse(y, mu):
 	return np.linalg.norm(y - mu) / np.sqrt(len(y))
 
 # file with field data
-# data_file = 'data/plant_width_mean_dataset.pkl'
-# data_file = 'data/plant_height_mean(cm)_dataset.pkl'
-# data_file = 'data/height_aerial(cm)_dataset.pkl'
-data_file = 'data/plant_count_mean_dataset.pkl'
+feature = 'plant_width_mean'
+# feature = 'plant_height_mean(cm)'
+# feature = 'height_aerial(cm)'
+# feature = 'plant_count_mean'
+data_file = 'data/' + feature + '_dataset.pkl'
 env = FieldEnv(data_file=data_file)
 
 num_rows, num_cols = env.shape
 n = env.num_samples
-num_train = int(.7*n)
+num_train = int(.6*n)
 train_indices = np.random.randint(0, n, num_train)
 test_indices = np.array(list(set(range(n)) - set(train_indices)))
 
@@ -58,9 +97,9 @@ y_test = y[test_indices]
 # y_train = env.collect_samples(train_indices, noise_std)
 # y_train = transform_y(y_train)
 
-gp1 = SklearnGPR()
-mu1, std1, rmse1 = fit_and_eval(gp1, x, y, x_train, y_train)
-test_rmse1 = evaluate(gp1, x_test, y_test)
+# gp1 = SklearnGPR()
+# mu1, std1, rmse1 = fit_and_eval(gp1, x, y, x_train, y_train)
+# test_rmse1 = evaluate(gp1, x_test, y_test)
 
 lr = .1
 gp2 = GpytorchGPR(latent=None, lr=lr, kernel='matern')
@@ -81,16 +120,19 @@ kwargs_field = {'rr_dim': 2, 'v_dim': x.shape[-1]-2}
 mu_field, std_field, rmse_field = fit_and_eval(gp_field, x, y, x_train, y_train, **kwargs_field)
 test_rmse_field = evaluate(gp_field, x_test, y_test)
 
-print('GP_sklearn RMSE', test_rmse1)
+# print('GP_sklearn RMSE', test_rmse1)
 print('GP_simple RMSE', test_rmse2)
 # print('GP_linear RMSE', rmse3)
 # print('GP_non_linear RMSE', rmse4)
 print('GP_field', test_rmse_field)
 
-fig1 = draw_plots(num_rows, num_cols, y, mu1, std1**2, 'GP_sklearn')
+# fig1 = draw_plots(num_rows, num_cols, y, mu1, std1**2, 'GP_sklearn')
 fig2 = draw_plots(num_rows, num_cols, y, mu2, std2**2, 'GP_simple')
 # fig3 = draw_plots(num_rows, num_cols, y, mu3, std3**2, 'GP_linear')
 # fig4 = draw_plots(num_rows, num_cols, y, mu4, std4**2, 'GP_non_linear')
-fig5 = draw_plots(num_rows, num_cols, y, mu_field, std_field**2, 'GP_field')
+# fig5 = draw_plots(num_rows, num_cols, y, mu_field, std_field**2, 'GP_field')
+
+# fig = draw_3_plots(num_rows, num_cols, y, mu2, mu_field, feature)
 plt.show()
+
 

models.py
@@ -11,6 +11,8 @@ from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.random_variables import GaussianRandomVariable
 
 import numpy as np
+from scipy.linalg import cholesky, cho_solve, solve_triangular
+import warnings
 import ipdb
 from utils import to_torch
 
@@ -210,11 +212,12 @@ class GpytorchGPR(object):
         self._train_x = torch.FloatTensor(x)
         self._train_y = torch.FloatTensor(y)
         self._train_y_mean = self._train_y.mean()
+        self._norm_train_y = self._train_y - self._train_y_mean
         if var is not None:
             self._train_var = torch.FloatTensor(var)
 
         self.likelihood = GaussianLikelihood()
-        self.model = ExactGPModel(self._train_x, self._train_y,
+        self.model = ExactGPModel(self._train_x, self._norm_train_y,
                                   self.likelihood, self._train_var,
                                   self.latent, self.kernel, **kwargs)
 
@@ -225,37 +228,49 @@ class GpytorchGPR(object):
         # self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,
                             # mode='min',patience=50,verbose=True)
             
-
     def fit(self, x, y, var=None, **kwargs):
+        if var is None:
+            var = np.full(len(y), 1e-5)
         self.reset(x, y, var, **kwargs)
         self.model.train()
         self.likelihood.train()
 
-        training_iter = 500
-        norm_train_y = self._train_y - self._train_y_mean
+        training_iter = 200
         
         # NOTE: with zero mean, loss seems to converge
+        # TODO: no. of iterations depends on the actual values
+        # it may be a good idea to normalize the output values (check this)
         for i in range(training_iter):
             self.optimizer.zero_grad()
             output = self.model(self._train_x)
-            loss = -self.mll(output, norm_train_y)#/len(y)
+            loss = -self.mll(output, self._norm_train_y)#/len(y)
             loss.backward()
             self.optimizer.step()
             # self.lr_scheduler.step(loss)
             print(i, loss.item())
-        
-    def cov_mat(self, x1, x2, white_noise=None):
+
+        # precomputing quantities for predictions
+        K = self.cov_mat(self._train_x, white_noise=self._train_var)
+        self.L_ = cholesky(K, lower=True)
+
+    def cov_mat(self, x1, x2=None, white_noise=None):
         x1_ = to_torch(x1)
         x2_ = to_torch(x2)
-        # important to set model to eval mode
+        white_noise = to_torch(white_noise)
+        flag = x2_ is None or torch.equal(x1_, x2_)
+
+        # NOTE: set model to eval mode
         self.model.eval()
         with torch.no_grad():
             x1_ = self.model.latent_func(x1_)
-            x2_ = self.model.latent_func(x2_)
-            cov = self.model.covar_module(x1_, x2_).evaluate().cpu().numpy()
-        # add white noise component here
-        if white_noise is not None:
-            cov = cov + white_noise
+            if flag:
+                cov = self.model.covar_module(x1_).evaluate().cpu().numpy()
+                # add white noise only for symmetric case
+                if white_noise is not None:
+                    cov = cov + torch.diag(white_noise)
+            else:
+                x2_ = self.model.latent_func(x2_)
+                cov = self.model.covar_module(x1_, x2_).evaluate().cpu().numpy()
         return cov
 
     def predict(self, x, return_cov=False, return_std=False):
@@ -267,11 +282,25 @@ class GpytorchGPR(object):
             pred = self.likelihood(self.model(x_))
             pred_mean = pred.mean().cpu().numpy()
             pred_mean += self._train_y_mean
+
+            K_trans = self.cov_mat(x_, self._train_x)
             if return_std:
-                std = pred.std().cpu().numpy()
-                return pred_mean, std
+                L_inv = solve_triangular(self.L_.T, np.eye(self.L_.shape[0]))
+                K_inv = L_inv.dot(L_inv.T)
+
+                y_var = np.ones(x.shape[0])
+                y_var -= np.einsum('ij,ij->i', np.dot(K_trans, K_inv), K_trans)
+                y_var_negative = y_var < 0
+                if np.any(y_var_negative):
+                    warnings.warn("Predicted variances smaller than 0. "
+                                  "Setting those variances to 0.")
+                    y_var[y_var_negative] = 0.0
+                ipdb.set_trace()
+                return pred_mean, np.sqrt(y_var)
+
             elif return_cov:
-                cov = pred.covar().evaluate().cpu().numpy()
-                return pred_mean, cov
-            else:
-                return pred_mean
+                v = cho_solve((self.L_, True), K_trans.T)
+                y_cov = self.cov_mat(x) - K_trans.dot(v)
+                return pred_mean, y_cov
+
+            return pred_mean