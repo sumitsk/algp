major cleanup | fixed bugs, bottleneck entropy computation

graph_utils.py
@@ -60,20 +60,17 @@ def in_between(node, down_node, up_node):
     return False
     
 
-def lower_bound_path_cost(node, waypoints):
-    # minimum cost of traversal to all the unvisited waypoints
-    # pose = node[0]
+def lower_bound_path_cost(pose, waypoints):
+    # return minimum cost of traversal to all the unvisited waypoints
 
-    # remaining waypoints
-    left = [w for i,w in enumerate(waypoints) if not node[1][i]]
-    all_locs = np.vstack(left + [node[0]])
     # boundaries of the bounding box
+    all_locs = np.vstack(waypoints + [pose])
     # TODO: this can be further tightened :
     # 1. if only one waypoint left (find the exact distance to go)
     # 2. if there are waypoints in columns other than the robot's, then use nearest junction for bounding box computation
     mins = np.min(all_locs, axis=0)
     maxs = np.max(all_locs, axis=0)
-    t = np.array(node[0])
+    t = np.array(pose)
     a = t - mins
     b = maxs - t
     dist = sum(a) + sum(b) + min(a[0],b[0]) + min(a[1],b[1])
@@ -142,16 +139,11 @@ def path_cost(path):
     return cost
 
 
-def node_action(tree, node):
-    # determines what to do with the given node
-    # all nodes in the graph with same position and orientation
-    visiteds = [n[1] for n in tree.nodes() if n[0]==node[0] and n[2]==node[2]]
-    for v in visiteds:
-        diff = np.array(v)*1 - np.array(node[1])*1
-        # if same node exists, then merge this node
-        if (diff==0).all():
-            return 'merge'
-        # if better node exists then this node need not be added if max path length = shortest path length
-        if (diff>=0).all() and diff.max()==1:
-            return 'discard'
-    return 'keep'
+def find_merge_to_node(tree, node):
+    # all nodes in the graph with same attributes as node
+    all_nodes = [n for n in tree.nodes() if tree.node[n]==node]
+    if len(all_nodes) > 1:
+        ipdb.set_trace()
+    if len(all_nodes) > 0:
+        return all_nodes[0]
+    return None   

map.py
@@ -1,18 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import ipdb
-from utils import manhattan_distance, BFSNode, is_valid_cell
-from copy import deepcopy
+from utils import manhattan_distance
 from graph_utils import get_heading, opposite_headings
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import seaborn as sns
 
 
 class Map(object):
-    def __init__(self,
-                 num_gp_rows=15,
-                 num_gp_cols=37,
-                 num_row_passes=2,
-                 row_pass_width=1
-                 ):
+    def __init__(self, num_gp_rows=15, num_gp_cols=37, num_row_passes=2, row_pass_width=1):
 
         super(Map, self).__init__()
         
@@ -26,24 +22,25 @@ class Map(object):
         self._shape = self._compute_map_dimensions()
         self.stack_len = self.num_gp_rows // (self.num_row_passes + 1)
         self.row_pass_indices = self._get_row_pass_indices()
-        self.obstacle_cols = np.arange(0, self.shape[1], 2)
-        
+        self.free_cols = np.arange(0, self.shape[1], 2)
+        self.obstacle_cols = np.arange(1, self.shape[1], 2)
+
         # 1 if obstacle 0 otherwise
-        self.occupied = self._build_occupancy_grid()
+        self.occupied = self._set_occupancy_grid()
 
         # all poses (x,y) in the map
-        x, y = np.meshgrid(np.arange(self._shape[1]), np.arange(self._shape[0]))
-        self.all_poses = np.vstack([y.flatten(), x.flatten()]).T
+        # x, y = np.meshgrid(np.arange(self._shape[1]), np.arange(self._shape[0]))
+        # self.all_poses = np.vstack([y.flatten(), x.flatten()]).T
 
     @property
     def shape(self):
         return self._shape
 
-    def _build_occupancy_grid(self):
-        # generates the occupancy grid
+    def _set_occupancy_grid(self):
+        # returns the occupancy grid of the map
         grid = np.full(self._shape, False)
         nr, nc = self._shape
-        grid[:, 1:nc:2] = True
+        grid[:, self.obstacle_cols] = True
         grid[self.row_pass_indices, :] = False
         return grid
 
@@ -63,68 +60,9 @@ class Map(object):
             t += 1
             last = last + self.row_pass_width + self.stack_len
         return np.array(ind)
-
-    # TODO: make this the defacto rendering method and remove others
-    def render(self, gp_pose=None, fig_idx=None, paths=None, locs=None):
-        fig_idx = 0 if fig_idx is None else fig_idx
-        plt.figure(fig_idx)
-        plt.title('Field Map')
-        plot = 1.0 - np.repeat(self.occupied[:, :, np.newaxis], 3, axis=2)
-        if gp_pose is not None:
-            map_pos = self.gp_pose_to_map_pose(gp_pose)
-            plt.scatter(map_pos[1], map_pos[0])
-
-        if paths is not None:
-            plot = 1.0 - np.repeat(self.occupied[:, :, np.newaxis], 3, axis=2)
-            # highlight all paths
-            for path in paths:
-                plot[path[:, 0], path[:, 1], :] = [.75, .75, .5]
-
-        plt.imshow(plot, cmap='gray', interpolation='none', aspect='equal')
-        plt.show()
-
-    def gp_pose_to_map_pose(self, gp_pose):
-        # convert gp_pose to map_pose 
-        if isinstance(gp_pose, tuple):
-            poses = np.array(gp_pose)
-        else:
-            poses = np.copy(gp_pose)
-        poses = poses.reshape(-1, 2)
-        map_poses = np.zeros(poses.shape)
-        map_poses[:, 1] = 2 * poses[:, 1]
-        a = poses[:, 0] // self.stack_len
-        b = poses[:, 0] % self.stack_len + self.row_pass_width
-        map_poses[:, 0] = a * (self.stack_len + self.row_pass_width) + b
-        map_poses = map_poses.astype(int)
-        if isinstance(gp_pose, tuple):
-            return tuple(map_poses.squeeze())
-        else:
-            return map_poses
-
-    def map_pose_to_gp_pose(self, map_pose):
-        # convert map pose to gp pose 
-        assert isinstance(map_pose, tuple), 'Map pose must be a tuple!'
-        if map_pose[1] % 2 == 1:
-            return None
-        if map_pose[0] in self.row_pass_indices:
-            return None
-
-        y = map_pose[1] // 2
-        x = map_pose[0] - np.sum(self.row_pass_indices < map_pose[0])
-        return x, y
-
-    # TODO: remove if not needed anymore
-    @staticmethod
-    def get_distances(pose, map_poses):
-        raise NotImplementedError('This is wrong')
-        # distance between two nodes won't be simply manhattan distance
-        # pose_ = np.array(pose).reshape(-1, 2)
-        # map_poses_ = np.array(map_poses).reshape(-1, 2)
-        # return np.abs(pose_ - map_poses_).sum(axis=-1)
-
+   
     def distance_between_nodes(self, start, goal, heading):
         # return distance between start and goal and final heading on goal
-
         # if start and goal are in the same column
         if start[1] == goal[1]:
             h = get_heading(goal, start)
@@ -134,19 +72,44 @@ class Map(object):
                 return manhattan_distance(start, goal), heading
             # if not, move to the junction, then move to the adjacent column (and come back later) and proceed to the goal
             else:
-                node = self.get_junction(start, heading)    
-                total_dist = manhattan_distance(start, node) + 2*2 + manhattan_distance(node, goal)
-                return total_dist, (-heading[0], 0)
+                sj = self.get_junction(start, heading)    
+                gj = self.get_junction(goal, heading)
+
+                # start and goal in different blocks
+                if sj!=gj:
+                    total_dist = manhattan_distance(start, sj) + 2*2 + manhattan_distance(sj, goal)
+                    return total_dist, (-heading[0], 0)
+                # start and goal in same block, move to the opposite junction first and then straight to goal
+                else:
+                    node = self.get_junction(goal, (-heading[0], 0))
+                    total_dist = manhattan_distance(start, sj) + 2*2 + manhattan_distance(sj, node) + manhattan_distance(node, goal)
+                    return total_dist, heading
 
         # start and goal are in different columns
         # move to the junction and then proceed to the goal
         else:
-            node = self.get_junction(start, heading)
-            total_dist = manhattan_distance(start, node) + manhattan_distance(node, goal)
-            # shift to the column which has the goal and compute new heading
-            final_heading = get_heading(goal, (node[0], goal[1]))
-            final_heading = heading if final_heading is None else final_heading
-            return total_dist, final_heading
+            if heading in [(1,0), (-1,0)]:
+                node = self.get_junction(start, heading)
+                total_dist = manhattan_distance(start, node) + manhattan_distance(node, goal)
+                # shift to the column which has the goal and compute new heading
+                final_heading = get_heading(goal, (node[0], goal[1]))
+                final_heading = heading if final_heading is None else final_heading
+                return total_dist, final_heading
+            else:
+                up_node = self.get_up_junction(goal)
+                down_node = self.get_down_junction(goal)
+                # go to down node if up node lies in the same row as start
+                if start[1] == up_node[1]:
+                    total_dist = manhattan_distance(start, down_node) + manhattan_distance(down_node, goal)
+                    final_heading = (-1,0)
+                # go to up node if down node lies in the same row as start
+                elif start[1] == down_node[1]:
+                    total_dist = manhattan_distance(start, up_node) + manhattan_distance(up_node, goal)
+                    final_heading = (1,0)
+                else:
+                    total_dist = manhattan_distance(start, goal)
+                    final_heading = get_heading(goal, (start[0], goal[1]))
+                return total_dist, final_heading
 
     def get_junction(self, pose, heading):
         # return junction in the heading direction
@@ -155,6 +118,8 @@ class Map(object):
         elif heading == (-1,0):
             return self.get_up_junction(pose)
         else:
+            return pose
+            # print(pose, heading)
             raise NotImplementedError
 
     def get_up_junction(self, pose):
@@ -169,7 +134,6 @@ class Map(object):
 
     def nearest_waypoint_path_cost(self, start, start_heading, waypoints):
         # return cost of the path formed by always moving to the nearest waypoint
-        # NOTE: this serves as an upper bound on the path cost
         nw = len(waypoints)
         visited = [False]*nw
         total_cost = 0
@@ -194,177 +158,54 @@ class Map(object):
             visited[idx] = True
         return total_cost
 
-    # TODO: remove if not required
-    def all_paths(self, start, waypoints, delta_input=None, max_length=None):
-        # waypoints - list of tuples
-        start = tuple(start)
-        n_waypoints = len(waypoints)
-        # location of waypoints
-        w_mask = np.full(self.shape, False)
-        for w in waypoints:
-            w_mask[w] = True
+    # def _render_path(self, ax):
+    #     # ax.set_cmap('hot')
+    #     plot = 1.0 - np.repeat(self.env.map.occupied[:, :, np.newaxis], 3, axis=2)
+    #     # highlight camera measurement
+    #     if self.path.shape[0] > 0:
+    #         plot[self.path[:, 0], self.path[:, 1], :] = [.75, .75, .5]
+    #     # highlight sensor measurement
+    #     if self.sensor_seq.shape[0] > 0:
+    #         plot[self.sensor_seq[:, 0], self.sensor_seq[:, 1]] = [.05, 1, .05]
 
-        if max_length is None:
-            # max_length = 2.5 * minimum_distance_to_go(start, waypoints)
-            max_length = 1 * self.min_dist_to_goal(start, waypoints[0], delta_input) + self.stack_len
+    #     plot[self.agent_map_pose[0], self.agent_map_pose[1], :] = [0, 0, 1]
+    #     ax.set_title('Environment')
+    #     ax.imshow(plot)
 
-        cost = 1
-        dx_dy = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    # def render(self, fig, ax, pred, var):
+    #     # TODO: improve render by drawing arrows and annotating
+    #     # render path
+    #     self._render_path(ax[0, 0])
 
-        node = BFSNode(pose=start, gval=0, visited=np.full(n_waypoints, False))
-        open_nodes = [node]
-        closed_nodes = []  
-        while len(open_nodes) > 0:
-            node = open_nodes.pop(0)
-            # if node is a valid sampling location, then we do not need to do delta additions here, we can jump to intersection
-            
-            if len(node.path)>=2:
-                delta = node.pose[0] - node.path[-2][0]
-            else:
-                delta = delta_input
+    #     # TODO: use seaborn for rendering
+    #     # render plots
+    #     axt, axp, axv = ax[1, 0], ax[1, 1], ax[0, 1]
+    #     axt.set_title('Ground Truth / Actual values')
+    #     imt = axt.imshow(self.env.full_Y, cmap='ocean', vmin=0, vmax=self.env.Y.max())
+    #     div = make_axes_locatable(axt)
+    #     caxt = div.new_horizontal(size='5%', pad=.05)
+    #     fig.add_axes(caxt)
+    #     fig.colorbar(imt, caxt, orientation='vertical')
 
-            gp_pose = self.map_pose_to_gp_pose(node.pose)
-            if gp_pose is not None:
-                # because of no U-turn there will be only one way extension
-                
-                # move agent until an intersection
-                while gp_pose is not None:
-                    node.gval += cost
-                    if node.gval > max_length:
-                        break 
-                    assert delta is not None, 'delta can not be None'
-                    node.pose = (node.pose[0] + delta, node.pose[1])                    
-                    node.path.append(node.pose)
-                    if w_mask[node.pose]:
-                        node.visited[waypoints.index(node.pose)] = True
-                        # print('waypoint visited')
-                        # print(node.__dict__)
+    #     axp.set_title('Predicted values')
+    #     res_pred = self.env.vec_to_gp_mat(pred)
+    #     imp = axp.imshow(res_pred, cmap='ocean', vmin=0, vmax=self.env.Y.max())
+    #     divm = make_axes_locatable(axp)
+    #     caxp = divm.new_horizontal(size='5%', pad=.05)
+    #     fig.add_axes(caxp)
+    #     fig.colorbar(imp, caxp, orientation='vertical')
 
-                    if node.visited.all():
-                        closed_nodes.append(node)
-                        break
-                    gp_pose = self.map_pose_to_gp_pose(node.pose)
-                # insert into open nodes only if it has reached an intersection
-                if gp_pose is None:
-                    open_nodes.append(node)
-
-            else:
-                for dxdy in dx_dy:
-                    new_pose = (node.pose[0] + dxdy[0], node.pose[1] + dxdy[1])
-
-                    # within bounds and unoccupied
-                    if not is_valid_cell(new_pose, self.shape) or self.occupied[new_pose]:
-                        continue
-
-                    # don't expand in the reverse direction (robot can't take a U-turn)
-                    if len(node.path)>=2 and new_pose==node.path[-2]:
-                        continue
-
-                    new_gval = node.gval + cost 
-
-                    # for only 1 waypoint (essentially if there is a goal location)
-                    # to_go = self.min_dist_to_goal(new_pose, waypoints[0], delta) > max_length
-                    to_go = manhattan_distance(new_pose, waypoints[0])
-                    if new_gval + to_go > max_length:
-                        continue
-                    # doesn't exceed maximum length
-                    # remaining_waypoints = [loc for i,loc in enumerate(waypoints) if not node.visited[i]]
-                    # if new_gval + minimum_distance_to_go(new_pose, remaining_waypoints) > max_length:
-                    #     continue
-                    
-                    # if there is a loop and it doesn't contain any waypoint then remove it
-                    # NOTE: an intersection can never be a waypoint
-                    if new_pose in node.path:
-                        # last index of new_pose in node.path
-                        idx = len(node.path) - 1 - node.path[::-1].index(new_pose)
-                        loop = node.path[idx:]
-                        if not has_waypoints(loop, waypoints):
-                            continue
-
-                    new_visited = np.copy(node.visited)
-                    new_path = deepcopy(node.path)
-                    new_path.append(new_pose)
-                    new_node = BFSNode(new_pose, new_gval, new_visited, new_path)
-                    if w_mask[new_node.pose]:
-                        new_node.visited[waypoints.index(new_node.pose)] = True
-                        # print('waypoint reached')
-                        # print(new_node.__dict__)
-
-                    if new_node.visited.all():
-                        closed_nodes.append(new_node)
-                    else:
-                        open_nodes.append(new_node)
-
-        paths = [np.stack(node.path) for node in closed_nodes]
-        # there should not be any duplicates
-        # if duplicate_paths(paths):
-        #     ipdb.set_trace()
-        # ipdb.set_trace()
-        return paths
-
-    # not required anymore
-    def find_loops(self, path):
-        visited = np.full(self.shape, False)
-        for node in path:
-            # loop detected
-            if visited[tuple(node)] is True:
-                ipdb.set_trace()
-            visited[tuple(node)] = True
-        print(len(path), visited.sum())
-
-    # TODO: remove if not required
-    def min_dist_to_goal(self, pose, goal, delta=None):
-        if delta is None:
-            return manhattan_distance(pose, goal)
-
-        if pose[0] in self.row_pass_indices:
-            if pose[1]%2==1:
-                return manhattan_distance(pose, goal)
-            else:
-                next_pose = (pose[0] + delta, pose[1])
-                return manhattan_distance(next_pose, goal) + 1
-
-        x = pose[0]
-        while x not in self.row_pass_indices:
-            x += delta
-        next_pose = (x, pose[1])
-        return abs(x-pose[0]) + manhattan_distance(next_pose, goal)
-    
-        # find downward intersection
-        # if delta==1:
-
-        # # find upward intersection
-        # elif delta==-1:
-
-        # else:
-        #     raise NotImplementedError
-
-
-def has_waypoints(path, waypoints):
-    if waypoints is None:
-        return False
-    for loc in waypoints:
-        if loc in path:
-            return True
-    return False
-
-
-def duplicate_paths(paths):
-    # paths is a list of numpy arrays
-    for i in range(len(paths)):
-        for j in range(i+1, len(paths)):
-            p1 = paths[i]
-            p2 = paths[j]
-            if p1.shape == p2.shape and (p1==p2).all():
-                return True
-    return False
+    #     axv.set_title('Variance')
+    #     res_var = self.env.vec_to_gp_mat(var)
+    #     imv = axv.imshow(res_var, cmap='hot')
+    #     # divv = make_axes_locatable(axv)
+    #     # caxv = divv.new_horizontal(size='5%', pad=.05)
+    #     # fig.add_axes(caxv)
+    #     # fig.colorbar(imv, caxv, orientation='vertical')
 
 
 if __name__ == '__main__':
-    small_map = Map(num_gp_rows=15,
-                    num_gp_cols=15,
-                    num_row_passes=2,
-                    row_pass_width=1)
+    small_map = Map(num_gp_rows=15, num_gp_cols=15, num_row_passes=2, row_pass_width=1)
 
     waypoints = [(7,6), (15,16), (11,10), (5,10)]
     for w in waypoints:
@@ -373,6 +214,5 @@ if __name__ == '__main__':
     start = (0,0)
     heading = (1,0)
     path_length = small_map.nearest_waypoint_path_cost(start, heading, waypoints)
-    ipdb.set_trace()
 
 

models.py
@@ -16,14 +16,8 @@ import warnings
 import ipdb
 from utils import to_torch, to_numpy
 from pprint import pprint
-
-
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1 or classname.find('Linear') != -1:
-        nn.init.orthogonal_(m.weight.data)
-        if m.bias is not None:
-            m.bias.data.fill_(0)
+import matplotlib.pyplot as plt
+from copy import deepcopy
 
 
 class SklearnGPR(object):
@@ -70,52 +64,6 @@ class SklearnGPR(object):
         return cov
 
     
-class ExactGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood, var=None, latent=None, kernel_params=None, latent_params=None):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
-        self._set_latent_function(latent, latent_params)
-        
-        self.mean_module = ZeroMean()
-        ard_num_dims = self.latent_func.embed_dim if self.latent_func.embed_dim is not None else train_x.size(-1)
-        
-        kernel = kernel_params['type'] if kernel_params is not None else 'rbf'
-        if kernel is None or kernel == 'rbf':
-            self.kernel_covar_module = RBFKernel(ard_num_dims=ard_num_dims)
-        elif kernel == 'matern':
-            self.kernel_covar_module = MaternKernel(nu=1.5, ard_num_dims=ard_num_dims)
-        elif kernel == 'spectral_mixture':
-            self.kernel_covar_module = SpectralMixtureKernel(n_mixtures=kernel_params['n_mixtures'], n_dims=train_x.size(-1))
-            self.kernel_covar_module.initialize_from_data(train_x, train_y)
-        else:
-            raise NotImplementedError
-
-        # set covariance module
-        if var is not None:
-            self.noise_covar_module = WhiteNoiseKernel(var)
-            self.covar_module = self.kernel_covar_module + self.noise_covar_module
-        else:
-            self.covar_module = self.kernel_covar_module
-        
-    def _set_latent_function(self, latent, latent_params):
-        if latent is None or latent == 'identity':
-            self.latent_func = IdentityLatentFunction()
-        elif latent == 'linear':
-            self.latent_func = LinearLatentFunction(latent_params['input_dim'], latent_params['embed_dim'])
-        elif latent == 'non_linear':
-            self.latent_func = NonLinearLatentFunction(latent_params['input_dim'], latent_params['embed_dim'], latent_params['embed_dim'])
-        # NOTE: not using for now
-        # elif latent == 'field':
-        #     self.latent_func = FieldLatentFunction(latent_params['input_dim'], )
-        else:
-            raise NotImplementedError
-
-    def forward(self, inp):
-        x = self.latent_func(inp)
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return GaussianRandomVariable(mean_x, covar_x)
-
-
 class IdentityLatentFunction(nn.Module):
     def __init__(self):
         super(IdentityLatentFunction, self).__init__()
@@ -205,9 +153,6 @@ class GpytorchGPR(object):
         self.kernel_params = kernel_params
         self.latent_params = latent_params
         self.max_iter = max_iterations
-        # args = {'lr': lr, 'max_iter': max_iterations}
-        # print('GPR model arguments:')
-        # pprint(args)
 
     @property
     def train_x(self):
@@ -217,7 +162,7 @@ class GpytorchGPR(object):
     def train_var(self):
         return self._train_var.cpu().numpy()
 
-    def reset(self, x, y, var):
+    def reset(self, x, y, var, load_hyperparams=False):
         self._train_x = to_torch(x)
         self._train_y = to_torch(y)
         self._train_y_mean = self._train_y.mean()
@@ -229,10 +174,12 @@ class GpytorchGPR(object):
         self.model = ExactGPModel(self._train_x, self._norm_train_y, self.likelihood, self._train_var, self.latent, self.kernel_params, self.latent_params)
         self.optimizer = torch.optim.Adam([{'params': self.model.parameters()}, ], lr=self.lr)
         self.mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
-        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min',patience=50,verbose=True)
-            
+        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', patience=50, verbose=True)
+        if load_hyperparams:
+            ipdb.set_trace()
+
+
     def fit(self, x, y, var=None, disp=False):
-        #  TODO: do we need non-zero var (probably yes for cholesky decomposition)
         if var is None:
             var = np.full(len(y), 1e-5)
         self.reset(x, y, var)
@@ -240,6 +187,7 @@ class GpytorchGPR(object):
         self.likelihood.train()
         
         # NOTE: with zero mean and low range observation (0-1 types), loss stabilises
+        losses = []
         for i in range(self.max_iter):
             self.optimizer.zero_grad()
             output = self.model(self._train_x)
@@ -253,27 +201,29 @@ class GpytorchGPR(object):
                 initial_ll = -loss.item()
             elif i == self.max_iter - 1:
                 final_ll = -loss.item()
+            losses.append(loss.item())
 
-        print('Initial LogLikelihood {:.3f} Final LogLikelihood {:.3f}'.format(initial_ll, final_ll))
+        # print('Initial LogLikelihood {:.3f} Final LogLikelihood {:.3f}'.format(initial_ll, final_ll))
+        # print(self.optimizer.param_groups[0]['lr'])
+        # pr = [x for x in self.model.named_parameters()]
+        # print(dict(pr)['kernel_covar_module.log_lengthscale'])
+        # embed = self.get_embeddings(self._train_x)
         # precomputing quantities for predictions
-        K = self.cov_mat(self._train_x, white_noise=self._train_var)
+        K = self.cov_mat(self._train_x) + np.diag(self.train_var)
         self.L_ = cholesky(K, lower=True)
+        # K = self.L_ * self.L_.T
+        # self.hyperparams = deepcopy(dict(self.model.named_parameters()))
 
-    def cov_mat(self, x1, x2=None, white_noise=None):
+    def cov_mat(self, x1, x2=None):
         x1_ = to_torch(x1)
         x2_ = to_torch(x2)
-        white_noise = to_torch(white_noise)
-        flag = x2_ is None or torch.equal(x1_, x2_)
-
+        
         # NOTE: set model to eval mode
         self.model.eval()
         with torch.no_grad():
             x1_ = self.model.latent_func(x1_)
-            if flag:
+            if x2_ is None or torch.equal(x1_, x2_):
                 cov = self.model.covar_module(x1_).evaluate().cpu().numpy()
-                # add white noise only for symmetric case
-                if white_noise is not None:
-                    cov = cov + torch.diag(white_noise)
             else:
                 x2_ = self.model.latent_func(x2_)
                 cov = self.model.covar_module(x1_, x2_).evaluate().cpu().numpy()
@@ -315,3 +265,52 @@ class GpytorchGPR(object):
             x_ = to_torch(x)
             embeds = self.model.latent_func(x_)
             return to_numpy(embeds)
+
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, var=None, latent=None, kernel_params=None, latent_params=None):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        if latent_params is None:
+            latent_params = {'input_dim': train_x.size(-1)}
+        self._set_latent_function(latent, latent_params)
+        
+        self.mean_module = ZeroMean()
+        ard_num_dims = self.latent_func.embed_dim if self.latent_func.embed_dim is not None else train_x.size(-1)
+        
+        kernel = kernel_params['type'] if kernel_params is not None else 'rbf'
+        if kernel is None or kernel == 'rbf':
+            self.kernel_covar_module = RBFKernel(ard_num_dims=ard_num_dims)
+        elif kernel == 'matern':
+            self.kernel_covar_module = MaternKernel(nu=1.5, ard_num_dims=ard_num_dims)
+        elif kernel == 'spectral_mixture':
+            self.kernel_covar_module = SpectralMixtureKernel(n_mixtures=kernel_params['n_mixtures'], n_dims=train_x.size(-1))
+            self.kernel_covar_module.initialize_from_data(train_x, train_y)
+        else:
+            raise NotImplementedError
+
+        # set covariance module
+        if var is not None:
+            self.noise_covar_module = WhiteNoiseKernel(var)
+            self.covar_module = self.kernel_covar_module + self.noise_covar_module
+        else:
+            self.covar_module = self.kernel_covar_module
+        
+    def _set_latent_function(self, latent, latent_params):
+        if latent is None or latent == 'identity':
+            self.latent_func = IdentityLatentFunction()
+        elif latent == 'linear':
+            if 'embed_dim' not in latent_params:
+                latent_params['embed_dim'] = 6
+            self.latent_func = LinearLatentFunction(latent_params['input_dim'], latent_params['embed_dim'])
+        elif latent == 'non_linear':
+            if 'embed_dim' not in latent_params:
+                latent_params['embed_dim'] = 6
+            self.latent_func = NonLinearLatentFunction(latent_params['input_dim'], latent_params['embed_dim'], latent_params['embed_dim'])
+        else:
+            raise NotImplementedError
+
+    def forward(self, inp):
+        x = self.latent_func(inp)
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return GaussianRandomVariable(mean_x, covar_x)

utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import torch
 import pickle 
+import pandas as pd
 
 
 CONST = .5*np.log(2*np.pi*np.exp(1))
@@ -53,7 +54,22 @@ def load_data(filename):
     num_cols = data_dict['num_cols']
     X = data_dict['X']
     Y = data_dict['Y'].squeeze()
-    return num_rows, num_cols, X, Y
+    valid = data_dict['valid'].squeeze()
+    return num_rows, num_cols, X[valid], Y[valid]
+
+
+def load_dataframe(filename, target_feature, extra_input_features=[]):
+    df = pd.read_pickle(filename)
+    num_rows = 15
+    num_cols = 37
+    X = np.vstack(df[['X']].values.squeeze())
+    row_range = X[:, :2]
+    gene = X[:, 2:]
+    ph_vals = np.hstack([df[[f]].values for f in extra_input_features])
+    final_x = np.concatenate([row_range, ph_vals, gene], axis=1)
+    y = df[[target_feature]].values.squeeze()
+    genotype = df[['category']].values.squeeze()
+    return num_rows, num_cols, final_x, y, genotype
 
 
 def generate_gaussian_data(num_rows, num_cols, k=5, min_var=10, max_var=100, algo='sum'):
@@ -271,3 +287,22 @@ def fit_and_eval(gp, train_x, train_y, test_x, test_y, disp=False):
     train_rmse = compute_rmse(train_y, pred_train)
     test_rmse = compute_rmse(test_y, pred_test)
     return train_rmse, test_rmse
+
+
+def valid_neighbors(pose, grid_shape, dxdy=None):
+    if dxdy is None:
+        dxdy=[(0,1), (0,-1), (1,0), (-1,0)]
+    nodes = []
+    for dx,dy in dxdy:
+        new_node = (pose[0] + dx, pose[1] + dy) 
+        if is_valid_cell(new_node, grid_shape):
+            nodes.append(new_node)
+    return nodes
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1 or classname.find('Linear') != -1:
+        torch.nn.init.orthogonal_(m.weight.data)
+        if m.bias is not None:
+            m.bias.data.fill_(0)